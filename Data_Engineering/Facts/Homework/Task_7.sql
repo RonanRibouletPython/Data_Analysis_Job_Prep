aa990276332f03e216a0a9b241383db1a701e3d9
RonanRibouletPython <newsletterpython13@gmail.com>
2024-12-12 21:28:00 +0100

    Task 7 done

---
+++ Data_Engineering/Facts/Homework/Task_7.sql
@@ -0,0 +1,15 @@
+-- Task 7: Create the DDL host_activity_reduced
+create table host_activity_reduced (
+	month_start date,
+	host text,
+	hit_array INTEGER[],
+    unique_visitors_array INTEGER[],
+    primary key(host, month_start)
+)
+
+--drop table host_activity_reduced;
+--delete from host_activity_reduced;
+
+
+
+
